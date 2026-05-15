2e5e985b48ac8113ee20d06c67e92c5f22df3fa6
thisisroni <thisisroni@gapp.nthu.edu.tw>
2026-05-16 04:30:04 +0800

    fix go test

--- services/api/internal/ticketing/ticket_holder.go
+++ services/api/internal/ticketing/ticket_holder.go
@@ -4,6 +4,6 @@ func ticketHolderFromTicket(ticket Ticket) TicketHolder {
 	return TicketHolder{
 		DisplayName: ticket.EmployeeName,
 		Department:  ticket.Department,
-		City:        ticket.City,
+		City:        normalizeLocation(ticket.City),
 	}
 }
